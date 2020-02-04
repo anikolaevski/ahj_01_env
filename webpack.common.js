34892404776b7dd185b2b8bcb7991a66e213ef37
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2020-02-04 20:46:23 +0300

    Task#1 done

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const APP_DIR = path.resolve(__dirname, "./src");
-const BUILD_DIR = path.resolve(__dirname , "dist");
+
+const APP_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
@@ -8,11 +9,12 @@ module.exports = {
   output: {
     path: BUILD_DIR,
   },
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        include : APP_DIR + '/js',
+        include: `${APP_DIR}/js`,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -25,14 +27,14 @@ module.exports = {
             loader: 'html-loader',
             options: {
               root: './src/index.html',
-            }
+            },
           },
         ],
       },
       {
         test: /\.css$/,
         use: [
-          MiniCssExtractPlugin.loader, 
+          MiniCssExtractPlugin.loader,
           'css-loader',
         ],
       },
@@ -52,7 +54,14 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin(),
-    new MiniCssExtractPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: 'src/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,14 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common');
-const TerserPlugin = require('terser-webpack-plugin');
+
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const common = require('./webpack.common');
+
+// const path = require('path');
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
